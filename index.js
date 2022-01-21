2cb72793a1503f43b548036ae24285fae9b1ecd6
moises2702 <moisescamacho26@gmail.com>
2022-01-21 00:40:06 -0400

    Test_cloudinary_2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const sql = require('./src/database/database');
+const cloudinary = require('cloudinary');
 
 app.use(express.static('build'));
 app.use(express.json());
